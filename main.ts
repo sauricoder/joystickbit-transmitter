a1e4eab5cce759e190a4eab9526a1f0507be8d9a
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-05-17 09:25:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+joystickbit.initJoystickBit()
+radio.setGroup(9)
+basic.showString("T")
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 10) {
+        radio.setGroup(4)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 1010) {
+        radio.setGroup(3)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 10) {
+        radio.setGroup(2)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 1010) {
+        radio.setGroup(1)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.setGroup(5)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.setGroup(6)
+    } else {
+        radio.setGroup(0)
+    }
+})
